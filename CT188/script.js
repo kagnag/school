8ba68bbe004202af60c4cd41da1793de9c9e0a61
huynhbaokhang <73773630+kagnag@users.noreply.github.com>
2021-11-26 12:12:49 +0700

    add js

--- CT188/script.js
+++ CT188/script.js
@@ -1,19 +1,60 @@
-function lienhe(frm) {
+function checkKeySearch(e) {
+  var key = event.which | event.keyCode;
+  if (key == 32) {
+    doSearch();
+  }
+}
+
+function doSearch() {
+  var frm = document.forms["frm-search"];
+  if (frm.words.value.length > 0) frm.submit();
+}
+
+function showSearch() {
+  var url = new URL(window.location);
+  var ws = url.searchParams.get("words");
+  document.getElementById("searchDetail").innerHTML =
+    "<h1>Từ khoá tìm kiếm</h1> <b>" + ws + "</b>";
+}
+
+// bai tap 2
+function loginValidate(frm) {
   var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  if (emailReg.test(email.value) == false) {
-    /*mã xử lý dữ liệu không hợp lệ*/
+  if (emailReg.test(frm.email.value) == false) {
+    alert("Vui lòng nhập lại email hợp lệ");
+    frm.email.focus();
     return false;
   }
-  if (psw.value.length < 8) {
-    /*mã xử lý dữ liệu không hợp lệ*/
+  if (frm.psw.value.length < 8) {
+    alert("Mật khẩu có tối thiểu 8 ký tự");
+    frm.psw.focus();
     return false;
   }
-  if (hoten.value.length < 4) {
-    /*mã xử lý dữ liệu không hợp lệ*/
+
+  alert("Đã gửi dữ liệu!");
+  return true;
+}
+
+function registerValidate(frm) {
+  var emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (emailReg.test(frm.email.value) == false) {
+    alert("Vui lòng nhập lại email hợp lệ");
+    frm.email.focus();
     return false;
   }
-  if (lienhe.value.length < 10) {
-    /*mã xử lý dữ liệu không hợp lệ*/
+  if (frm.psw.value.length < 8) {
+    alert("Mật khẩu có tối thiểu 8 ký tự");
+    frm.psw.focus();
+    return false;
+  }
+  if (frm.psw2.value.length < 8) {
+    alert("Mật khẩu có tối thiểu 8 ký tự");
+    frm.psw2.focus();
+    return false;
+  }
+  if (frm.psw.value.length != frm.psw2.value.length) {
+    alert("Mật khẩu và Nhập lại mật khẩu phải giống nhau");
+    frm.psw.focus();
     return false;
   }
   alert("Đã gửi dữ liệu!");
@@ -68,12 +109,60 @@ var itemList = {
   },
 };
 
-function sanpham(frm) {
+function addCart(code) {
+  var number = parseInt(document.getElementById(code).value);
+  var name = itemList[code].name;
+  if (number == 0) return;
   if (typeof localStorage[code] === "undefined") {
     window.localStorage.setItem(code, number);
+  } else {
+    var current = parseInt(window.localStorage.getItem(code));
+    if (current + number > 100) {
+      window.localStorage.setItem(code, 100);
+      alert(
+        "Mỗi mặt hàng chỉ có thể đặt 100 sản phẩm cho mỗi đơn hàng. Bạn đã đặt 100 sản phẩm"
+      );
+      return;
+    } else window.localStorage.setItem(code, current + number);
   }
-  window.localStorage.setItem(code, 100);
-  number = parseInt(document.getElementById(code).value);
-  current = parseInt(window.localStorage.getItem(code));
-  window.localStorage.setItem(code, current + number);
+  alert(
+    "Đã cập nhật sản phẩm" +
+      name +
+      " với số lượng " +
+      number +
+      " vào giỏ hàng. Số lượng sản phẩm là..." +
+      number
+  );
+}
+
+function openCart() {
+  window.location.href = "donhang.html";
+}
+
+function showCart() {
+  var formatter = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  });
+  var container = document
+    .getElementById("cartDetail")
+    .getElementsByTagName("tbody")[0];
+  container.innerHTML = "";
+  var sum = 0;
+  totalPreTax = 0;
+  discountRate = getDiscountRate();
+  taxRate = 0;
+  discount = 0;
+  tax = 0;
+  for (var i = 0; i < window.localStorage.length; i++) {
+    if (typeof itemList[localStorage.key(i)] === "undefined") continue;
+    var tr = document.createElement("tr");
+    photoCell = document.createElement("td");
+    nameCell = document.createElement("td");
+    priceCell = document.createElement("td");
+    numberCell = document.createElement("td");
+    photoCell = document.createElement("td");
+    removeCell = document.createElement("td");
+    removeLink = document.createElement("a");
+  }
 }
